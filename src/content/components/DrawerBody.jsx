da36341222c2dc92e8e2bc70ba4fa774a8388292
joesekai <theakhilsabuofficial@gmail.com>
2023-04-19 19:48:22 +0530

    feat: tabs and empty screen

---
+++ src/content/components/DrawerBody.jsx
@@ -0,0 +1,42 @@
+import { Fragment } from "preact";
+import { useState } from "preact/hooks";
+import { values } from "lodash-es";
+
+import Tab from "./Tab";
+import { TABS } from "../constants";
+import InsectSVG from "./svg/InsectSVG";
+import MessageLayout from "./MessageLayout";
+
+export default function DrawerBody() {
+  const [selectedTab, setSelectedTab] = useState(TABS.RECENT.key);
+
+  return (
+    <Fragment>
+      <input
+        className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="Search for an Icon or Image ✨"
+      />
+      <div class="w-full max-w-md">
+        <div class="flex justify-between space-x-4">
+          {values(TABS).map((tab) => (
+            <Tab
+              key={tab.key}
+              Icon={tab.icon}
+              label={tab.label}
+              isSelected={selectedTab === tab.key}
+              handleClick={() => setSelectedTab(tab.key)}
+            />
+          ))}
+        </div>
+        <div class="mt-6">
+          <MessageLayout
+            SVG={InsectSVG}
+            title="Oops! We're cricketing a blank here!"
+            description="chirp... chirp... chirp..."
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
+}
--- src/content/index.js
+++ src/content/index.js
@@ -1,1 +1,40 @@
 import "./index.css";
+
+import App from "./App";
+import { render } from "preact";
+
+const container = document.createElement("div");
+container.id = "visual-valet-container";
+document.body.appendChild(container);
+
+const toolbar = document.querySelector(".Island > .Stack_horizontal");
+
+// Add divider between other tools and the custom drawer button
+const divider = document.createElement("div");
+divider.className = "App-toolbar__divider";
+
+// Code for the drawer button
+const label = document.createElement("label");
+label.className = "ToolIcon Shape";
+label.title = "Visual Valet — cmd or ctrl";
+label.id = "visual-valet-button";
+
+const input = document.createElement("input");
+input.className = "ToolIcon_type_radio ToolIcon_size_medium";
+input.type = "radio";
+input.name = "editor-current-shape";
+input.setAttribute("aria-label", "Visual Valet");
+input.setAttribute("aria-keyshortcuts", "cmd or ctrl");
+input.setAttribute("data-testid", "toolbar-visual-valet");
+
+const div = document.createElement("div");
+div.className = "ToolIcon__icon";
+div.innerHTML = `<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M2 5m0 2a2 2 0 0 1 2 -2h16a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-16a2 2 0 0 1 -2 -2z"></path><path d="M12 12m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path><path d="M9 12h-7"></path><path d="M15 12h7"></path></svg>`;
+
+label.appendChild(input);
+label.appendChild(div);
+
+toolbar.appendChild(divider);
+toolbar.appendChild(label);
+
+render(<App />, container);
